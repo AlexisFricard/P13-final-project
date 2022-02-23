69c20cff4b3b3c91086d92983f128ac62840d8dc
Fricard Alexis <a.fricardpro@gmail.com>
2022-02-23 19:01:46 +0100

    push to test bug static root

--- mastercontrat/settings.py
+++ mastercontrat/settings.py
@@ -147,65 +147,70 @@ else:
         }
     }
 
-# AWS SETTINGS (ScaleWay)
-AWS_ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv("BUCKET_NAME")
-AWS_S3_REGION_NAME = os.getenv('S3_REGION_NAME')
+if os.getenv('ENV') != "GITHUB":
+    # AWS SETTINGS (ScaleWay)
+    AWS_ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv("BUCKET_NAME")
+    AWS_S3_REGION_NAME = os.getenv('S3_REGION_NAME')
 
-AWS_DEFAULT_ACL = 'public-read'
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_LOCATION = 'static'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
 
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# AWS STATIC SETTINGS
-AWS_S3_HOST = 's3.%s.scw.cloud' % (AWS_S3_REGION_NAME,)
-AWS_S3_ENDPOINT_URL = 'https://%s' % (AWS_S3_HOST,)
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # AWS STATIC SETTINGS
+    AWS_S3_HOST = 's3.%s.scw.cloud' % (AWS_S3_REGION_NAME,)
+    AWS_S3_ENDPOINT_URL = 'https://%s' % (AWS_S3_HOST,)
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# AWS MEDIA SETTINGS
-PUBLIC_MEDIA_LOCATION = 'media/'
-MEDIA_URL = (
-    f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}' +
-    f'/{PUBLIC_MEDIA_LOCATION}'
-)
+    # AWS MEDIA SETTINGS
+    PUBLIC_MEDIA_LOCATION = 'media/'
+    MEDIA_URL = (
+        f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}' +
+        f'/{PUBLIC_MEDIA_LOCATION}'
+    )
 
-# TODO: Add file storage_backends in frontpage/
-DEFAULT_FILE_STORAGE = 'frontpage.storage_backends.PublicMediaStorage'
+    # TODO: Add file storage_backends in frontpage/
+    DEFAULT_FILE_STORAGE = 'frontpage.storage_backends.PublicMediaStorage'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    # Default primary key field type
+    # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# TODO: Email service for passwords_reset and
-#       send user username & password & contact
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "L'association des M2 Contrats de Poitiers"
+    # TODO: Email service for passwords_reset and
+    #       send user username & password & contact
+    EMAIL_HOST = os.environ.get("EMAIL_HOST")
+    EMAIL_PORT = os.environ.get("EMAIL_PORT")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = "L'association des M2 Contrats de Poitiers"
 
-# Keep our policy as strict as possible
-# CSP_DEFAULT_SRC = ("'self'", f'{STATIC_URL}', f'{MEDIA_URL}')
-# CSP_STYLE_SRC = ("'self'", f'{STATIC_URL}')
-CSP_SCRIPT_SRC = ("'self'", f'{STATIC_URL}')
-# CSP_FONT_SRC = ("'self'", f'{STATIC_URL}')
-# CSP_IMG_SRC = (
-#   "'self'", f'{STATIC_URL}',
-#   'data: maps.gstatic.com *.googleapis.com *.ggpht.com'
-# )
+    # Keep our policy as strict as possible
+    # CSP_DEFAULT_SRC = ("'self'", f'{STATIC_URL}', f'{MEDIA_URL}')
+    # CSP_STYLE_SRC = ("'self'", f'{STATIC_URL}')
+    CSP_SCRIPT_SRC = ("'self'", f'{STATIC_URL}')
+    # CSP_FONT_SRC = ("'self'", f'{STATIC_URL}')
+    # CSP_IMG_SRC = (
+    #   "'self'", f'{STATIC_URL}',
+    #   'data: maps.gstatic.com *.googleapis.com *.ggpht.com'
+    # )
 
-# For tchat
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-        },
-}
+    # For tchat
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            "CONFIG": {
+                "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            },
+            },
+    }
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_URL = '/media/'
