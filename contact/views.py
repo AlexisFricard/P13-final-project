4e337209518ccb3b2184ad91d13affdf10404319
Fricard Alexis <a.fricardpro@gmail.com>
2022-02-24 18:32:59 +0100

    add response after send email

--- contact/views.py
+++ contact/views.py
@@ -5,17 +5,32 @@ from django.core.mail import send_mail
 # Send email
 def contact(request):
     if request.method == "POST":
-        send_mail(
-            f'{request.POST.get("object")}',
-            (
-                f'Nom : {request.POST.get("name")}\n' +
-                f'Email : {request.POST.get("email")}\n' +
-                f'Tél : {request.POST.get("phone_number")}' +
-                f'\n\nMessage :\n\n{request.POST.get("message")}'
-            ),
-            'noreply@masteraffairescontrats-poitiers.fr',
-            ['contact@masteraffairescontrats-poitiers.fr'],
-            fail_silently=False,
-        )
-        return render(request, "contact.html", {'email_send': True})
+        response = True
+        text = 'L\'email n\'as pas été envoyé'
+        error = 1
+        try:
+            send_mail(
+                f'{request.POST.get("object")}',
+                (
+                    f'Nom : {request.POST.get("name")}\n' +
+                    f'Email : {request.POST.get("email")}\n' +
+                    f'Tél : {request.POST.get("phone_number")}' +
+                    f'\n\nMessage :\n\n{request.POST.get("message")}'
+                ),
+                'noreply@masteraffairescontrats-poitiers.fr',
+                ['contact@masteraffairescontrats-poitiers.fr'],
+                fail_silently=False,
+            )
+
+            text = 'L\'email as été envoyé'
+            error = 0
+        except ConnectionError:
+            text = 'L\'email n\'as pas été envoyé'
+            error = 1
+
+        return render(request, "contact.html", {
+            'response': response,
+            'response_text': text,
+            'response_error': error,
+        })
     return render(request, "contact.html")
