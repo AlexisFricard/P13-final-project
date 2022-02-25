fd8c46916da738f4770653d3c51afd261e347803
Fricard Alexis <a.fricardpro@gmail.com>
2022-02-25 16:38:23 +0100

    debug session test 2 (reset password)

--- mastercontrat/settings.py
+++ mastercontrat/settings.py
@@ -189,7 +189,9 @@ if os.getenv('ENV') != "GITHUB":
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
     EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = "Association des Master Contrats de Poitiers"
+    DEFAULT_FROM_EMAIL = 'Association des Master Contrats de Poitiers'
+
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 
     # Keep our policy as strict as possible
     # CSP_DEFAULT_SRC = ("'self'", f'{STATIC_URL}', f'{MEDIA_URL}')
